3c03af3ee10feb012623bf5528187ee1f8bb9831
Archit Warghane <archit2x3@hotmail.com>
2020-02-06 19:38:14 +0530

    example API file

---
+++ test-app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+
+const app = express();
+
+//basic
+app.get('/basic',(req, res) => {
+  res.send('I am basc Route');
+});
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Hello From the server Side',
+    app: 'Natours'
+  });
+});
+
+//Post Data
+app.post('/', (req, res) => {
+
+var t =(req.query);
+  res.json({
+   // message : 'You can Post to this End-Point',
+  message:t
+  });
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`app running on port ${port}`);
+});
+
+/*
+URL Designing
+tours (plural)
+/addNewTour => POST /tours
+/getTour    => GET /tours
+/updateTour => [PUT/PATCH] /tours (put : have to send all resources, patch: need to send resources that needs to get updated)
+/deleteTour => DELETE /tours
+
+ */
