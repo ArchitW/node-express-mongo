eef7d1bc1bdec39c95fa6cf41049df487ac8f1b7
Archit Warghane <archit2x3@hotmail.com>
2020-02-08 12:52:30 +0530

    ref

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,11 +7,21 @@ router.param('id', (req, res,next, val) => {
  console.log('ID=>' + val);
  next();
 });
+//create a middleware
+//check for name and price
+// 400 for bad request
 
+ middleware = ((req , res, next) => {
+ if (!req.body.name || !req.body.price){
+  return res.status(400).json({status:'fail',message:'missing name and price'})
+ }
+  next();
+
+});
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.postATour);
+  .post(middleware, tourController.postATour);
 
 
 router
